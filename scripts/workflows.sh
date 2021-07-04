602a8ce95dfd8ff90cc915d39b2f4ee94c954fdb
Carlos A Becker <caarlos0@gmail.com>
2021-07-04 00:48:37 +0000

    fix: workflow
    Signed-off-by: Carlos A Becker <caarlos0@gmail.com>

--- scripts/workflows.sh
+++ scripts/workflows.sh
@@ -6,19 +6,17 @@ on:
   push:
   pull_request:
 
-jobs:
-  build:
+jobs:" >.github/workflows/check.yml
+
+for f in *.yml; do
+	echo "  $f:
     runs-on: ubuntu-latest
     steps:
       - name: checkout
-        uses: actions/checkout@v2" >.github/workflows/check.yml
-
-for f in *.yml; do
-	echo "
-      - name: goreleaser check $f
+        uses: actions/checkout@v2
+      - name: check
         uses: goreleaser/goreleaser-action@v2
         with:
           distribution: goreleaser-pro
           args: check -q -f $f" >>.github/workflows/check.yml
 done
-
